50d696a5bdaaac1cf1f08ad0184ecf35764a8b12
Lucio Tolentino (milano) <seanluciotolentino@gmail.com>
2014-03-07 11:29:35 -0600

    Added MainSingle for running a single population on the cluster

---
+++ simpactpurple/MainSingle.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 11:14:02 2013
+
+@author: Lucio
+
+A script which runs a single community simulation. 
+
+"""
+
+from mpi4py import MPI
+import Community
+import sys
+
+print "hello from", MPI.Get_processor_name(),"rank",MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+#MPI variables
+if __name__ == '__main__':
+    c = Community.Community()
+    c.INITIAL_POPULATION = int(sys.argv[1])
+    c.NUMBER_OF_YEARS = 30
+    c.run()
+
+print "exit"
