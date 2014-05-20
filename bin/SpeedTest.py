fc6ad696cab8b15e3e78cbd59d334d00397db563
seanluciotolentino <seanluciotolentino@gmail.com>
2014-05-20 15:06:22 -0400

    Emmacommit

--- bin/SpeedTest.py
+++ bin/SpeedTest.py
@@ -4,9 +4,7 @@ Created on Wed Oct 23 14:16:27 2013
 
 @author: Lucio
 
-Program for timing the execution time of SimpactPurple with clusters. 
-Specifically the script is for running a simulation distributed across v2 & v3
-on the vinci cluster.
+Program for timing the execution time of single SimpactPurple community.
 
 """
 import time
@@ -16,14 +14,15 @@ population_ranges = range(10000,150001,10000)
 #population_ranges = range(100,501,100)
 #population_ranges = range(10000,30001,5000)
 
-years = 30
-print "population\truntime"
-for pop in population_ranges:
-    #single node version
-    start = time.time()
-    s = simpactpurple.Community()
-    s.INITIAL_POPULATION = pop
-    s.NUMBER_OF_YEARS = years
-    s.run()
-    elapsed_time = round(time.time() - start,2)
-    print pop,"\t",elapsed_time
+if __name__ == '__main__':
+    years = 30
+    print "population\truntime"
+    for pop in population_ranges:
+        #single node version
+        start = time.time()
+        s = simpactpurple.Community()
+        s.INITIAL_POPULATION = pop
+        s.NUMBER_OF_YEARS = years
+        s.run()
+        elapsed_time = round(time.time() - start,2)
+        print pop,"\t",elapsed_time
